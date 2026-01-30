be99cc28ee2b5a84eacd4a055b70e00398c68a34
Henry Schimke <henry@azule.info>
2026-01-30 12:22:46 -0600

    complete reset

--- db/migrations/00004_add_backup_flags_to_credentials.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE user_credentials ADD COLUMN backup_eligible BOOLEAN DEFAULT 0;
-ALTER TABLE user_credentials ADD COLUMN backup_state BOOLEAN DEFAULT 0;
--- static-web/js/config.js
+++
@@ -1,6 +0,0 @@
-const Config = {
-    API_BASE_URL: 'https://calorize.test',
-    DEBUG: true, // Set to false in production or via build process if we had one
-};
-
-export default Config;
